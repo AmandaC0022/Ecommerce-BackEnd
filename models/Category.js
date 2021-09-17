1d4d5ce7f12e24ad42ab699df478d41bb5d86f92
Amanda Morgan <AmandaC0022@gmail.com>
2021-09-17 13:36:59 -0500

    added columns to Category model

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,43 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,  
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      defaultValue: 10, 
+      validate: {
+        isNumeric: true, 
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category', 
+        key: 'id', 
+        unique: false, 
+      }
+    },
   },
   {
     sequelize,
