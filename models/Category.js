21e97aabf083ac0f567bdd219189a93475e05d81
Amanda Morgan <AmandaC0022@gmail.com>
2021-09-17 13:46:04 -0500

    all columns for models are added

--- models/Category.js
+++ models/Category.js
@@ -16,33 +16,6 @@ Category.init(
       type: DataTypes.STRING,
       allowNull: false
     },
-    category_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    price: {
-      type: DataTypes.DECIMAL,
-      allowNull: false,
-      validate: {
-        isDecimal: true,  
-      }
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false, 
-      defaultValue: 10, 
-      validate: {
-        isNumeric: true, 
-      }
-    },
-    category_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'category', 
-        key: 'id', 
-        unique: false, 
-      }
-    },
   },
   {
     sequelize,
